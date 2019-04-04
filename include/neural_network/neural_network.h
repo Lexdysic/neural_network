60f56534e7296542ff6f98b3334414a59cffa5b3
Lexdysic <lexdysic@gmail.com>
2019-04-03 22:09:41 -0700

    sketch out interface

--- include/neural_network/neural_network.h
+++ include/neural_network/neural_network.h
@@ -1,71 +1,22 @@
 
 #include <stdint.h>
-#include <map>
-#include <set>
+#include <vector>
 
+using Weight  = float;
+using Neuron  = float;
+using Layer   = std::vector<Neuron>;
+using Weights = std::vector<Weight>;
 
-namespace key_aware {
-//
-// StringView
-//
-class StringView {
+class NeuralNetwork {
 public:
-    StringView () noexcept = default;
-    StringView (const StringView &) noexcept = default;
-    StringView (const char * str) noexcept;
-    inline StringView (const char * str, const int32_t length) noexcept :
-        m_ptr(str),
-        m_length(length)
-    {
-    }
+    void Run (const Layer & input);
+    void Train (const Layer & input, const Layer & output);
 
-    StringView & operator= (const StringView &) noexcept = default;
-    
-    inline const char * Ptr () const noexcept {
-        return m_ptr;
-    }
-
-    inline int32_t Length () const noexcept {
-        return m_length;
-    }
-
-    inline const char & operator[] (const int32_t index) const noexcept {
-        return m_ptr[index];
-    }
+    void Read (FILE * file);
+    void Write (FILE * file);
 
 private:
-    const char * m_ptr    = nullptr;
-    int32_t      m_length = 0;
+    std::vector<Layer> m_hidden;
+    Layer              m_outputs;
+    Layer              m_delta;
 };
-
-
-//
-// Trie
-// 
-class Trie {
-public:
-
-    void Add (const StringView & str);
-
-    void Search (const StringView & prefix, std::set<std::string> * output);
-
-    inline int32_t GetCount () const { return m_count; }
-    inline bool IsEmpty () const { return !m_count; }
-
-private:
-    struct Node {
-        std::string            value;
-        std::map<char, Node *> children;
-    };
-
-    Node    m_root;
-    int32_t m_count = 0;
-};
-
-
-//
-// API
-//
-int32_t Distance (const StringView & from, const StringView & to);
-
-} // namespace key_aware
